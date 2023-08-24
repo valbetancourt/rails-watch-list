befde4183966888b6cf25ee34c06fd998390311e
Valeria Betancourt <vbhdz98@live.com.mx>
2023-08-24 13:54:26 -0600

    routes, controller, actions & view for List

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # index, s
+  resources :lists, only: [:index, :show, :new, :create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+require 'open-uri'
+require 'json'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies = JSON.parse(URI.open(url).read)['results']
+
+i = 0
+
+10.times do
+  movie = Movie.new(
+    title: movies[i]['title'],
+    overview: movies[i]['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movies[i]['poster_path']}",
+    rating: movies[i]['vote_average']
+  )
+  movie.save
+  i += 1
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
